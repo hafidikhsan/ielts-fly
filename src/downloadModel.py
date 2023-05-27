ed2e1087f20b0b71b3651b02e8ab5ba09f06817a
hafidikhsan <harifin315@gmail.com>
2023-05-27 20:19:26 +0700

    Edit chmod mode downloadModel.py

--- src/downloadModel.py
+++ src/downloadModel.py
@@ -1,9 +1,9 @@
 import requests
-import os
+import os, sys, stat
 
 def downloadModel(url: str, dest_folder: str):
     if not os.path.exists(dest_folder):
-        os.chmod('../models/')  # create folder if it does not exist
+        os.chmod('../models/', stat.S_IXUSR)  # create folder if it does not exist
 
     filename = url.split('/')[-1].replace(" ", "_")  # be careful with file names
     file_path = os.path.join(dest_folder, filename)
